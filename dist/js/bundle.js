3603d7d7f9f2298cc02d049ec4c07e9ead6f6edd
Jodi Parker <jodilparker@gmail.com>
2017-08-28 14:28:32 -0600

    fixed closure and immutability bugs

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -12,248 +12,133 @@ angular.module('noServer', ['ui.router', 'ui.select', 'ngSanitize']).config(func
 });
 'use strict';
 
-angular.module('noServer').controller('changeMeController', function ($scope, changeMeService, $stateParams, recipeListService, singleRecipeService, $interval) {
-    // hookup tests
+angular.module('noServer').controller('changeMeController', function ($scope, $stateParams, recipeListService, singleRecipeService, $interval) {
+    //      ╔══════════════════════════════════════╗
+    //      ║                TESTS                 ║
+    //      ╚══════════════════════════════════════╝
     $scope.controllerTest = "changeMe controller is working";
-    $scope.serviceTest = changeMeService.serviceTest;
     $scope.recipeListServiceTest = recipeListService.recipeListServiceTest;
     $scope.singleRecipeServiceTest = singleRecipeService.singleRecipeServiceTest;
 
-    // list of variables
+    //      ╔══════════════════════════════════════╗
+    //      ║              VARIABLES               ║
+    //      ╚══════════════════════════════════════╝
     var trl = [];
     var allTheRaw = [];
     var lastOfAll = [];
+    var calvin = [];
     $scope.recipe = trl[0];
-    $scope.theID;
     $scope.selectedRecipe; //two way binding
     $scope.qtyToMake = 1; //two way binding
+    $scope.iQty = 1; //two way binding
     $scope.id = function (selectedRecipe) {
-        // $scope.theRecipe(selectedRecipe.match(/\d/ig).join(''))
+        calvin.length = 0;
         trl.length = 0;
         allTheRaw.length = 0;
         lastOfAll.length = 0;
         $scope.getOList(selectedRecipe.match(/\d/ig).join(''));
     };
 
-    // functions
-    //Get a list of all recipes
-    $scope.listOfRecipes = function () {
-        recipeListService.getRecipeList().then(function (response) {
-            $scope.recipeCard = response.data;
+    //      ╔══════════════════════════════════════╗
+    //      ║              FUNCTIONS               ║
+    //      ╚══════════════════════════════════════╝
+    // ............| Get a list of all recipes
+    $scope.listOfRecipes = function (_) {
+        return recipeListService.getRecipeList().then(function (response) {
+            return $scope.recipeCard = response.data;
         });
     };
     $scope.listOfRecipes();
 
-    // Get a single recipe by ID
-    // $scope.theRecipe = function (id) {
-    //     singleRecipeService.getRecipe(id).then(function (response) {
-    //         $scope.thisRecipe = response.data
-    //         $scope.tree = response.data.tree
-    //         console.log($scope.thisRecipe)
-    //     })
-    // }
-
-    //   item.synths['1'].tree  
-
-    // show directive span if recipe has a url_type = 'recipe'
-    // $scope.isRecipe = function (urltype, obj) {
-    //     // console.log(`the obj is ${obj}`)
-    //     for(let key in obj){
-    //         // console.log(`the key in the obj is ${key}`)
-    //         $scope.synthID = obj[key]
-    //         // console.log(`the new obj should be ${$scope.synthID} but is actually ${obj[key]}`)
-    //         break;
-    //     }        
-    //     return urltype > 0
-    // }
-
-    //add tier1 objects to raw material array
-    // var rawArr = []
-    // $scope.rawMat = rawArr
-    // $scope.raw = function (name, qty, sName, sQty, recipeBool) {
-    //     if (recipeBool) {
-    //         rawArr.push(
-    //             {
-    //                 [sName]: sQty
-    //             }
-    //         )
-    //     }
-    //     else {
-    //         rawArr.push(
-    //             {
-    //                 [name]: qty
-    //             }
-    //         )
-    //     }
-
-    //     return rawArr //of raw objects and their quantities
-    // }
-
-    // combine duplicate mats
-    // var shoppingListArr = []
-    // $scope.testy = shoppingListArr
-    // $scope.startInterval = function(){
-    //     $interval(function(){
-    //         $scope.combineMats = function (rawArr) {
-    //             let a = rawArr;
-    //             let ans = {};
-    //             for (let i = 0; i < a.length; ++i) {
-    //                 for (let obj in a[i]) {
-    //                     ans[obj] = ans[obj] ? ans[obj] + a[i][obj] : a[i][obj];
-    //                 }
-    //             }
-    //             shoppingListArr.push(ans)
-    //         }
-    //         // console.log(rawArr)
-    //         $scope.combineMats(rawArr)
-    //     }, 500,1)  
-    // }
-
-    //make the shoppinListArr into a Json blob
-    // var jsonObject = []
-    // $scope.shoppingJson = jsonObject
-    // $scope.startIntervalMakeJson = function(){
-    //     $interval(function(){
-    //         $scope.makeJson = function (arr, newKeyName, newValueQty) {
-    //             let obj = arr[0]
-    //             // console.log(obj)
-    //             for (let key in obj) {
-    //                 // console.log(key)
-    //                 // console.log(obj[key])
-    //                 jsonObject.push(
-    //                     {
-    //                         [newKeyName]: key
-    //                         , [newValueQty]: obj[key]
-    //                     }
-    //                 )
-    //             }
-    //             // console.log(jsonObject)
-    //         }
-    //         // console.log(jsonObject)
-    //         $scope.makeJson(shoppingListArr, "name", "qty")
-    //     }, 1000,1)
-    // }
-
-
-    //Enable get recipe button
+    // ............| Enable get recipe button
     $scope.enabeGetRecipe = function (bool) {
         return $scope.getRecipeButton = bool;
     };
 
-    //get dynamic synths id
+    // ............| get and create recipe object
     $scope.getOList = function (id) {
         singleRecipeService.getRecipe(id).then(function (response) {
-            trl.push({
-                job: response.data.classjob.name,
-                icon: response.data.icon,
-                id: response.data.id,
-                name: response.data.name,
-                level: response.data.level,
-                type: response.data.url_type,
-                ingredients: []
-            });
+            trl.push({ job: response.data.classjob.name, icon: response.data.icon, id: response.data.id, name: response.data.name, level: response.data.level, type: response.data.url_type, ingredients: [] });
             response.data.tree.map(function (e, i) {
                 if (!e.connect_craftable) {
                     trl[0].ingredients.push({ name: e.name, qty: e.quantity, icon: e.icon });
                 } else {
-                    var synthOBJ = Object.keys(e.synths);
-                    var subID = synthOBJ[0];
+                    var synthOBJ = Object.keys(e.synths),
+                        subID = synthOBJ[0];
                     trl[0].ingredients.push({ name: e.name, qty: e.quantity, icon: e.icon, ingredients: [] });
                     singleRecipeService.getRecipe(subID).then(function (newRes) {
                         trl[0].ingredients[i].ingredients = newRes.data.tree.map(function (e) {
-                            return { name: e.name, qty: e.quantity, icon: e.icon };
+                            return { name: e.name, qty: e.quantity * trl[0].ingredients[i].qty, icon: e.icon };
                         });
                     });
                 }
             });
         });
         $interval(function (_) {
-            $scope.meWantRaw(id);
-        }, 500, 1);
-    };
-
-    $scope.meWantRaw = function (id) {
-        var bananasunday = function bananasunday(id) {
-            singleRecipeService.getRecipe(id).then(function (response) {
-                response.data.tree.map(function (e) {
-                    if (!e.connect_craftable) allTheRaw.push({ name: e.name, qty: e.quantity, icon: e.icon });else {
-                        var synthOBJ = Object.keys(e.synths),
-                            subID = synthOBJ[0];bananasunday(subID);
-                    }
-                });
-            });
-        };
-        bananasunday(id);
-        $interval(function () {
-            $scope.setMatches();
+            getRawMaterialsList(id);
         }, 800, 1);
     };
 
-    $scope.setMatches = function (_) {
-        // lastOfAll.length = 0
-        var a = allTheRaw;
-        var temp = [];
-
-        var _loop = function _loop(i) {
-            // console.log(`name from allTheRaw = ${JSON.stringify(a[i].name)}`)
-            if (temp.indexOf(a[i].name) == -1) {
-                temp.push(a[i].name);
-                lastOfAll.push(a[i]);
+    // ............| gets raw materials list and adds it to main object
+    var getRawMaterialsList = function getRawMaterialsList(id) {
+        trl[0].ingredients.filter(function (e, i) {
+            var newQty = e.qty;
+            if (!e.ingredients) {
+                allTheRaw.push(e);
             } else {
-                lastOfAll.map(function (e) {
-                    if (e.name == a[i].name) {
-                        e.qty = e.qty + a[i].qty;
+                e.ingredients.forEach(function (elem, ind) {
+                    allTheRaw.push(elem);
+                });
+            }
+        });
+
+        allTheRaw.forEach(function (elem) {
+            return calvin.push(Object.assign({}, elem));
+        });
+
+        $interval(function (_) {
+            setMatches();
+        }, 500, 1);
+    };
+
+    // ............| combines like materials
+    var setMatches = function setMatches(_) {
+        var temp = [];
+        calvin.forEach(function (elem) {
+            if (temp.indexOf(elem.name) == -1) {
+                temp.push(elem.name);
+                lastOfAll.push(elem);
+            } else {
+                lastOfAll.forEach(function (e) {
+                    if (e.name == elem.name) {
+                        e.qty = e.qty + elem.qty;
                     }
                 });
             }
-        };
-
-        for (var i = 0; i < a.length; ++i) {
-            _loop(i);
-        }
-        // console.log(`temp is: ${temp}`)
-        // console.log(`lastOfAll is: ${JSON.stringify(lastOfAll)}`)
-        // console.log(`this  is: ${JSON.stringify(allTheRaw)}`)
-        // console.log(lastOfAll)
+        });
         trl[0].rawList = lastOfAll;
         $scope.recipe = trl[0];
         console.log($scope.recipe);
     };
 });
-'use strict';
+"use strict";
 
-angular.module('noServer').service('changeMeService', function ($http) {
-    this.serviceTest = "changeMe service is working";
+angular.module("noServer").directive("tests", function (changeMeService, recipeListService, singleRecipeService) {
+    return {
+        templateUrl: "../../views/connectionTest.html"
+    };
+});
+"use strict";
 
-    // test data
-    this.recipeList = [{
-        name: "soup",
-        id: 1,
-        crystal: "fire",
-        ingredients: [{
-            name: "onion",
-            quantity: 5
-        }, {
-            name: "salt",
-            quantity: 2
-        }, {
-            name: "grilled Meat",
-            quantity: 2,
-            isRecipe: true,
-            id: 3,
-            crystal: "fire",
-            ingredients: [{
-                name: "sheep meat",
-                quantity: 1
-            }, {
-                name: "salt",
-                quantity: 1
-            }]
-        }]
-    }, {
-        name: "banana pie",
-        id: 2
-    }];
+angular.module("noServer").directive("strikeOut", function () {
+    return {
+        link: function link(scope, element, attribure) {
+            element.on("click", function () {
+                element.css('text-decoration', 'line-through');
+                element.css('text-decoration-color', 'white');
+            });
+        }
+    };
 });
 'use strict';
 
@@ -273,46 +158,6 @@ angular.module('noServer').service('singleRecipeService', function ($http) {
         return $http.get('https://api.xivdb.com/recipe/' + id).then(function (response) {
             return response;
         });
-    };
-});
-"use strict";
-
-angular.module("noServer").directive("tests", function (changeMeService, recipeListService, singleRecipeService) {
-    return {
-        templateUrl: "../../views/connectionTest.html"
-    };
-});
-'use strict';
-
-angular.module("noServer").directive('nestedNestedRecipe', function (recipeListService, singleRecipeService) {
-    return {
-        templateUrl: '../../views/nestedNestedRecipe.html'
-    };
-});
-'use strict';
-
-angular.module("noServer").directive('nestedRecipe', function (recipeListService, singleRecipeService) {
-    return {
-        templateUrl: '../../views/nestedRecipe.html'
-    };
-});
-'use strict';
-
-angular.module("noServer").directive('shoppingList', function (recipeListService, singleRecipeService) {
-    return {
-        templateUrl: '../../views/shoppingList.html'
-    };
-});
-"use strict";
-
-angular.module("noServer").directive("strikeOut", function () {
-    return {
-        link: function link(scope, element, attribure) {
-            element.on("click", function () {
-                element.css('text-decoration', 'line-through');
-                element.css('text-decoration-color', 'white');
-            });
-        }
     };
 });
 //# sourceMappingURL=bundle.js.map
--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,206 +1,103 @@
-angular.module('noServer').controller('changeMeController', function ($scope, changeMeService, $stateParams, recipeListService, singleRecipeService, $interval) {
-    // hookup tests
+angular.module('noServer').controller('changeMeController', function ($scope, $stateParams, recipeListService, singleRecipeService, $interval) {
+    //      ╔══════════════════════════════════════╗
+    //      ║                TESTS                 ║
+    //      ╚══════════════════════════════════════╝
     $scope.controllerTest = "changeMe controller is working"
-    $scope.serviceTest = changeMeService.serviceTest
     $scope.recipeListServiceTest = recipeListService.recipeListServiceTest
     $scope.singleRecipeServiceTest = singleRecipeService.singleRecipeServiceTest
-    
-    // list of variables
+
+    //      ╔══════════════════════════════════════╗
+    //      ║              VARIABLES               ║
+    //      ╚══════════════════════════════════════╝
     let trl = []
     let allTheRaw = []
     let lastOfAll = []
+    let calvin = []
     $scope.recipe = trl[0]
-        $scope.theID    
-        $scope.selectedRecipe //two way binding
-        $scope.qtyToMake = 1 //two way binding
-        $scope.id = (selectedRecipe) => {
-            // $scope.theRecipe(selectedRecipe.match(/\d/ig).join(''))
-            trl.length = 0
-            allTheRaw.length = 0
-            lastOfAll.length = 0
-            $scope.getOList(selectedRecipe.match(/\d/ig).join(''))
-        }
+    $scope.selectedRecipe //two way binding
+    $scope.qtyToMake = 1 //two way binding
+    $scope.iQty = 1 //two way binding
+    $scope.id = (selectedRecipe) => {
+        calvin.length = 0
+        trl.length = 0
+        allTheRaw.length = 0
+        lastOfAll.length = 0
+        $scope.getOList(selectedRecipe.match(/\d/ig).join(''))
+    }
 
+    //      ╔══════════════════════════════════════╗
+    //      ║              FUNCTIONS               ║
+    //      ╚══════════════════════════════════════╝
+    // ............| Get a list of all recipes
+    $scope.listOfRecipes = _ => recipeListService.getRecipeList().then(response => $scope.recipeCard = response.data)
+    $scope.listOfRecipes()
 
+    // ............| Enable get recipe button
+    $scope.enabeGetRecipe = bool => { return $scope.getRecipeButton = bool }
 
-        // functions
-        //Get a list of all recipes
-        $scope.listOfRecipes = function () {
-            recipeListService.getRecipeList().then(function (response) {
-                $scope.recipeCard = response.data
+    // ............| get and create recipe object
+    $scope.getOList = id => {
+        singleRecipeService.getRecipe(id).then(response => {
+            trl.push({ job: response.data.classjob.name, icon: response.data.icon, id: response.data.id, name: response.data.name, level: response.data.level, type: response.data.url_type, ingredients: [] })
+            response.data.tree.map((e, i) => {
+                if (!e.connect_craftable) {
+                    trl[0].ingredients.push({ name: e.name, qty: e.quantity, icon: e.icon })
+                }
+                else {
+                    let synthOBJ = Object.keys(e.synths), subID = synthOBJ[0]
+                    trl[0].ingredients.push({ name: e.name, qty: e.quantity, icon: e.icon, ingredients: [] })
+                    singleRecipeService.getRecipe(subID).then(newRes => {
+                        trl[0].ingredients[i].ingredients = newRes.data.tree.map(e => {
+                            return { name: e.name, qty: e.quantity * trl[0].ingredients[i].qty, icon: e.icon }
+                        })
+                    })
+                }
             })
-        }
-        $scope.listOfRecipes()
-
-        // Get a single recipe by ID
-        // $scope.theRecipe = function (id) {
-        //     singleRecipeService.getRecipe(id).then(function (response) {
-        //         $scope.thisRecipe = response.data
-        //         $scope.tree = response.data.tree
-        //         console.log($scope.thisRecipe)
-        //     })
-        // }
-
-    //   item.synths['1'].tree  
-
-        // show directive span if recipe has a url_type = 'recipe'
-        // $scope.isRecipe = function (urltype, obj) {
-        //     // console.log(`the obj is ${obj}`)
-        //     for(let key in obj){
-        //         // console.log(`the key in the obj is ${key}`)
-        //         $scope.synthID = obj[key]
-        //         // console.log(`the new obj should be ${$scope.synthID} but is actually ${obj[key]}`)
-        //         break;
-        //     }        
-        //     return urltype > 0
-        // }
-
-        //add tier1 objects to raw material array
-        // var rawArr = []
-        // $scope.rawMat = rawArr
-        // $scope.raw = function (name, qty, sName, sQty, recipeBool) {
-        //     if (recipeBool) {
-        //         rawArr.push(
-        //             {
-        //                 [sName]: sQty
-        //             }
-        //         )
-        //     }
-        //     else {
-        //         rawArr.push(
-        //             {
-        //                 [name]: qty
-        //             }
-        //         )
-        //     }
-
-        //     return rawArr //of raw objects and their quantities
-        // }
-
-        // combine duplicate mats
-        // var shoppingListArr = []
-        // $scope.testy = shoppingListArr
-        // $scope.startInterval = function(){
-        //     $interval(function(){
-        //         $scope.combineMats = function (rawArr) {
-        //             let a = rawArr;
-        //             let ans = {};
-        //             for (let i = 0; i < a.length; ++i) {
-        //                 for (let obj in a[i]) {
-        //                     ans[obj] = ans[obj] ? ans[obj] + a[i][obj] : a[i][obj];
-        //                 }
-        //             }
-        //             shoppingListArr.push(ans)
-        //         }
-        //         // console.log(rawArr)
-        //         $scope.combineMats(rawArr)
-        //     }, 500,1)  
-        // }
-
-        //make the shoppinListArr into a Json blob
-        // var jsonObject = []
-        // $scope.shoppingJson = jsonObject
-        // $scope.startIntervalMakeJson = function(){
-        //     $interval(function(){
-        //         $scope.makeJson = function (arr, newKeyName, newValueQty) {
-        //             let obj = arr[0]
-        //             // console.log(obj)
-        //             for (let key in obj) {
-        //                 // console.log(key)
-        //                 // console.log(obj[key])
-        //                 jsonObject.push(
-        //                     {
-        //                         [newKeyName]: key
-        //                         , [newValueQty]: obj[key]
-        //                     }
-        //                 )
-        //             }
-        //             // console.log(jsonObject)
-        //         }
-        //         // console.log(jsonObject)
-        //         $scope.makeJson(shoppingListArr, "name", "qty")
-        //     }, 1000,1)
-        // }
-
-        
-        //Enable get recipe button
-        $scope.enabeGetRecipe = function (bool) {
-            return $scope.getRecipeButton = bool
-        }
-        
-        //get dynamic synths id
-$scope.getOList = id => {
-    singleRecipeService.getRecipe(id).then(response => {
-        trl.push({
-            job: response.data.classjob.name
-            , icon: response.data.icon
-            , id: response.data.id
-            , name: response.data.name
-            , level: response.data.level
-            , type: response.data.url_type
-            , ingredients: []
         })
-        response.data.tree.map((e, i) => {
-            if(!e.connect_craftable){
-                trl[0].ingredients.push({name: e.name, qty: e.quantity, icon: e.icon})
+        $interval(_ => {
+            getRawMaterialsList(id)
+        }, 800, 1)
+    }
+
+    // ............| gets raw materials list and adds it to main object
+    const getRawMaterialsList = (id) => {
+        trl[0].ingredients.filter((e, i) => {
+            let newQty = e.qty
+            if (!e.ingredients) {
+                allTheRaw.push(e)
             }
             else {
-                let synthOBJ = Object.keys(e.synths)
-                let subID = synthOBJ[0]
-                trl[0].ingredients.push({name: e.name, qty: e.quantity, icon: e.icon, ingredients: []})
-                singleRecipeService.getRecipe(subID).then(newRes => {
-                    trl[0].ingredients[i].ingredients = newRes.data.tree.map(e => {
-                        return {name: e.name, qty: e.quantity, icon: e.icon}
-                    })
+                e.ingredients.forEach((elem, ind) => {
+                    allTheRaw.push(elem)
                 })
             }
         })
-    })
-    $interval(_ => {
-        $scope.meWantRaw(id)
-    }, 500, 1)
-}
+        
+        allTheRaw.forEach(elem => calvin.push(Object.assign({}, elem)))
 
-$scope.meWantRaw = (id) => {
-    const bananasunday = id => {
-        singleRecipeService.getRecipe(id).then(response => {
-            response.data.tree.map(e => {
-                if(!e.connect_craftable) allTheRaw.push({name: e.name, qty: e.quantity, icon: e.icon})
-                    else {let synthOBJ = Object.keys(e.synths), subID = synthOBJ[0]; bananasunday(subID)}        
-            })
+        $interval(_ => {
+            setMatches()
+        }, 500, 1)
+    }
+
+    // ............| combines like materials
+    const setMatches = _ => {
+        let temp = [];
+        calvin.forEach(elem => {
+            if (temp.indexOf(elem.name) == -1) {
+                temp.push(elem.name)
+                lastOfAll.push(elem)
+            }
+            else {
+                lastOfAll.forEach(e => {
+                    if (e.name == elem.name) {
+                        e.qty = e.qty + elem.qty
+                    }
+                })
+            }
         })
+        trl[0].rawList = lastOfAll
+        $scope.recipe = trl[0]
+        console.log($scope.recipe)
     }
-    bananasunday(id)
-    $interval(function(){
-        $scope.setMatches()
-    }, 800, 1)
-}
-
-$scope.setMatches = _ => {
-    // lastOfAll.length = 0
-    let a = allTheRaw;
-    let temp = [];
-    for (let i = 0; i < a.length; ++i) {
-        // console.log(`name from allTheRaw = ${JSON.stringify(a[i].name)}`)
-        if(temp.indexOf(a[i].name) == -1){
-            temp.push(a[i].name)
-            lastOfAll.push(a[i])
-        }
-        else {
-            lastOfAll.map(e =>{
-                if(e.name == a[i].name){
-                    e.qty = e.qty + a[i].qty
-                }
-            })
-        }
-    }
-    // console.log(`temp is: ${temp}`)
-    // console.log(`lastOfAll is: ${JSON.stringify(lastOfAll)}`)
-    // console.log(`this  is: ${JSON.stringify(allTheRaw)}`)
-    // console.log(lastOfAll)
-    trl[0].rawList = lastOfAll
-    $scope.recipe = trl[0]
-    console.log($scope.recipe)
-}
-
 })
