5d9b4e6e21c967e54d332691d33f827e09d64d4e
Jodi Parker <jodilparker@gmail.com>
2017-08-28 16:12:15 -0600

    changed port to hard coded to see if that fixes it

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const config = require('../config')
-const port = config.port
+const port = 8080
 const app = express()
 app.use(express.static('./'))
 app.use(bodyParser.json())
