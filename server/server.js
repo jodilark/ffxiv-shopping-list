519779f5de3cbf9c969cad38ea4d978e67836ab0
Jodi Parker <jodilparker@gmail.com>
2017-08-28 16:24:41 -0600

    removed config reference

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
-const config = require('../config')
+// const config = require('../config')
 const port = 8080
 const app = express()
 app.use(express.static('./'))
 app.use(bodyParser.json())
-app.use(session({resave: true, saveUninitialized: true, secret: config.sessionSecret}));
+app.use(session({resave: true, saveUninitialized: true, secret: 'supercalifragilisticexpialidocious'}));
 
 //db and node echo
 app.listen(port, _ => console.log(`listening on port ${port}`))
