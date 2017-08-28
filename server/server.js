2bf0fa098d0eb3f182adab210a6a122f7137fb83
Jodi Parker <jodilparker@gmail.com>
2017-08-28 16:30:05 -0600

    removed session

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = 8080
 const app = express()
 app.use(express.static('./'))
 app.use(bodyParser.json())
-app.use(session({resave: true, saveUninitialized: true, secret: 'supercalifragilisticexpialidocious'}));
+// app.use(session({resave: true, saveUninitialized: true, secret: 'supercalifragilisticexpialidocious'}));
 
 //db and node echo
 app.listen(port, _ => console.log(`listening on port ${port}`))
